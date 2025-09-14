e45c1d0b7326ac7204b393042003ff1a7d8fe6e6
Muhammad NDIATH <c.muhamad.f.ndiath@gmail.com>
2025-09-14 18:58:27 +0000

    creation des tables

---
+++ Voyage/database/migrations/2025_09_14_183400_create_event_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_users', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('event_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained();
+            $table->string('status'); // inscrit, en attente, annulé
+            $table->time('created_at')->nullable(); //date d'inscription à l'événement
+            $table->unique(['user_id', 'event_id']); // Un utilisateur ne peut s'inscrire qu'une seule fois à un événement
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_users');
+    }
+};
